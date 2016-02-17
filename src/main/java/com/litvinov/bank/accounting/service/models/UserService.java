5270a23a916caab2fde6b132e16f344897269d92
inetFight <d-litvinov@ukr.net>
2016-02-17 17:28:04 +0200

    1

---
+++ src/main/java/com/litvinov/bank/accounting/service/models/UserService.java
@@ -0,0 +1,85 @@
+package com.litvinov.bank.accounting.service.models;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+
+import com.litvinov.bank.accounting.service.hibernate.basics.dao.DAO;
+
+public class UserService extends DAO {
+
+	public User createUser(String username, int balance) throws Exception {
+		try {
+			begin();
+			User user = new User();
+			user.setName(username);
+			user.setBalance(balance);
+			getSession().save(user);
+			commit();
+			return user;
+		} catch (HibernateException e) {
+			rollback();
+			throw new Exception("Could not create user " + username, e);
+		}
+	}
+
+	public User retrieveUser(String username) throws Exception {
+
+		try {
+			begin();
+			Query q = getSession().createQuery("from User where name = :username");
+			q.setString("username", username);
+			User user = (User) q.uniqueResult();
+			commit();
+			return user;
+		} catch (HibernateException e) {
+			rollback();
+			throw new Exception("Could not get user " + username, e);
+		}
+	}
+
+	public List<User> getAllUsers() throws Exception {
+		try {
+			begin();
+			Query q = getSession().createQuery("from User");
+			List<User> list = q.list();
+			commit();
+			return list;
+		} catch (HibernateException e) {
+			rollback();
+			throw new Exception("Could not get users " + e);
+		}
+	}
+
+	public boolean checkUser(String username) {
+
+		begin();
+		Query q = getSession().createQuery("from User where name = :username");
+		q.setString("username", username);
+		List<User> user = q.list();
+		if (user.isEmpty()) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	public void addBalance(String username, int balance) {
+
+		try {
+			begin();
+			close();
+			User user = retrieveUser(username);
+			begin();
+			user.setBalance(balance);
+			update(user);
+			commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/main/java/com/litvinov/bank/accounting/service/resourse/Resourse.java
@@ -0,0 +1,47 @@
+package com.litvinov.bank.accounting.service.resourse;
+
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.litvinov.bank.accounting.service.models.User;
+import com.litvinov.bank.accounting.service.models.UserService;
+
+@Path("/balance/")
+public class Resourse {
+	UserService userService = new UserService();
+
+	@GET
+	@Path("/{username}")
+	@Produces(MediaType.APPLICATION_XML)
+	public User getUser(@PathParam("username") String username) throws Exception {
+		User user = userService.retrieveUser(username);
+		return user;
+	}
+
+	@PUT
+	@Path("/{username}/{balance}")
+	@Produces(MediaType.APPLICATION_XML)
+	public User getMessage(@PathParam("username") String username, @PathParam("balance") int balance) throws Exception {
+
+		if (userService.checkUser(username)) {
+			userService.addBalance(username, balance);
+			return userService.retrieveUser(username);
+		} else {
+			userService.createUser(username, balance);
+			return userService.retrieveUser(username);
+		}
+	}
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_XML)
+	public List<User> getUsers() throws Exception {
+		return userService.getAllUsers();
+	}
+
+}
